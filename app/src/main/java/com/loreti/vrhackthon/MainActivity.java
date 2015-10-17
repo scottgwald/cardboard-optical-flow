23ad02fb2f24ecd856598721e37d2fc6ce2dbd77
Luke Loreti <lhloreti@gmail.com>
2015-10-17 13:38:16 -0400

    initial commit. Webview w/ three.js

---
+++ app/src/main/java/com/loreti/vrhackthon/MainActivity.java
@@ -0,0 +1,79 @@
+package com.loreti.vrhackthon;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import com.google.vrtoolkit.cardboard.CardboardActivity;
+
+
+public class MainActivity extends CardboardActivity {
+
+    private static final String TAG = "MainActivity";
+    private WebView mWebView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.web);
+        WebSettings settings = mWebView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setAllowFileAccessFromFileURLs(true);
+        settings.setAllowUniversalAccessFromFileURLs(true);
+
+        // Enable debugging through Chrome
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WebView.setWebContentsDebuggingEnabled(true);
+        }
+
+        // Set a web view client and a chrome client
+        mWebView.setWebViewClient(new WebViewClient());
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            // Need to accept permissions to use the camera and audio
+            @Override
+            public void onPermissionRequest(final PermissionRequest request) {
+                Log.d(TAG, "onPermissionRequest");
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+                    @Override
+                    public void run() {
+                        request.grant(request.getResources());
+                    }
+                });
+            }
+        });
+        mWebView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    onCardboardTrigger();
+                return true;
+            }
+        });
+        // Load the local HTML file into the webview
+        mWebView.loadUrl("file:///android_asset/www/cardboard/index.html");
+
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    public void onCardboardTrigger() {
+        Log.i(TAG, "onCardboardTrigger");
+        mWebView.loadUrl("javascript:console.log('cardboardTrigger')");
+    }
+}
